2395ac777b6499fd38e7536007c24184b7c8e3ee
Pratima Bharti <pratimabhartisittu@gmail.com>
2023-07-11 13:29:19 +0530

    authenticationDone

---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post()
+{
+  return (
+    <div className="post">
+    <div className="image">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ24WfXpNMC2EjWMrWb0hV5-wPgfZVCBUsHQYqgk5V&s" alt="" />
+    </div>
+    <div className="texts">
+    <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an</h2>
+     <p className="info">
+     <a href="" className="author">Pratima</a>
+     <time> 2023-01-2</time>
+     </p>
+    <p  className='summary'>t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of </p>
+    </div>
+    </div>
+  );
+}
